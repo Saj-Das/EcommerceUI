e5b3a5c5e11ca509496fbef153273560b1c4da9d
sajal <bloodness11>
2018-07-14 02:41:53 -0700

    changes

---
+++ Controllers/Models/product.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Renova.Models
+{
+    public class product
+    {
+        public string Brand { get; set; }
+        public string Category { get; set; }
+        public string CostPrice { get; set; }
+        public string Detail { get; set; }
+        public string ProductName { get; set; }
+        public string Quantity { get; set; }
+        public string SellingPrice { get; set; }
+
+        public string Tags { get; set; }
+    }
+}
